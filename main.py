e501ba23cf803148c2d50c2dc08f319411de154c
jaitir <111525363+jaitir@users.noreply.github.com>
2026-03-23 17:34:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = "8735115748:AAEA2i6UIvF3ERxZHz_OSMT23kvmM9vGbyo"
-WEBHOOK_BASE_URL = os.getenv("telegram-bot-production-ce2b.up.railway.app", "").strip().rstrip("/")
+WEBHOOK_BASE_URL = "https://gimmemo.onrender.com"
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "gimmemo_webhook_secret")
 ADMIN_ID = 264354988
